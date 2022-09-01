67c976bd7bbc2585625f5137933463a1d5334f03
peredelkin <ep8kha@gmail.com>
2022-09-01 10:36:28 +0500

    битовые поля и регистры spi

--- inc/spi.h
+++ inc/spi.h
@@ -9,12 +9,16 @@
 #define INC_SPI_H_
 
 #include <assert.h>
+#include "defs.h"
 #include "stm32f4xx.h"
+
+//TODO: проверить размер каждой структуры
 
 #define SPI_CR1_T_SIZE 2
 #define SPI_CR2_T_SIZE 2
 #define SPI_SR_T_SIZE 2
-#define SPI_I2S_T_SIZE 2
+#define SPI_I2SCFGR_T_SIZE 2
+#define SPI_I2SPR_T_SIZE 2
 
 //Фаза
 enum _SPI_CR1_CPHA {
@@ -134,7 +138,8 @@ enum _SPI_CR1_BIDIMODE {
 
 typedef enum _SPI_CR1_BIDIMODE spi_cr1_bidimode_t;
 
-typedef struct __attribute__((packed)) SPI_CR1_BITS {
+//SPI control bits 1
+typedef struct PACKED SPI_CR1_BITS {
 	unsigned CPHA		:1; //Bit 0		Clock phase
 	unsigned CPOL		:1; //Bit 1		Clock polarity
 	unsigned MSTR		:1; //Bit 2		Master selection
@@ -152,6 +157,7 @@ typedef struct __attribute__((packed)) SPI_CR1_BITS {
 } spi_cr1_t;
 static_assert(sizeof(spi_cr1_t) == SPI_CR1_T_SIZE, "Invalid size of spi_cr1_t!");
 
+//SPI control register 1
 typedef union _SPI_CR1_REG {
 	uint16_t all;
 	struct SPI_CR1_BITS bit;
@@ -214,7 +220,9 @@ enum _SPI_CR2_TXEIE {
 
 typedef enum _SPI_CR2_TXEIE spi_cr2_txeie_t;
 
-typedef struct __attribute__((packed)) SPI_CR2_BITS {
+
+//SPI control bits 2
+typedef struct PACKED SPI_CR2_BITS {
 	unsigned RXDMAEN	:1; //Bit 0		Rx buffer DMA enable
 	unsigned TXDMAEN	:1; //Bit 1		Tx buffer DMA enable
 	unsigned SSOE		:1; //Bit 2		SS output enable
@@ -227,13 +235,15 @@ typedef struct __attribute__((packed)) SPI_CR2_BITS {
 } spi_cr2_t;
 static_assert(sizeof(spi_cr2_t) == SPI_CR2_T_SIZE, "Invalid size of spi_cr2_t!");
 
+//SPI control register 2
 typedef union _SPI_CR2_REG {
 	uint16_t all;
 	struct SPI_CR2_BITS bit;
 } SPI_CR2_REG;
 
 
-typedef struct __attribute__((packed)) SPI_SR_BITS {
+//SPI status bits
+typedef struct PACKED SPI_SR_BITS {
 	unsigned RXNE		:1; //Bit 0		Receive buffer not empty
 	unsigned TXE		:1; //Bit 1		Transmit buffer empty
 	unsigned CHSIDE		:1; //Bit 2		Channel side
@@ -247,13 +257,38 @@ typedef struct __attribute__((packed)) SPI_SR_BITS {
 } spi_sr_t;
 static_assert(sizeof(spi_sr_t) == SPI_SR_T_SIZE, "Invalid size of spi_sr_t!");
 
+//SPI status register
 typedef union _SPI_SR_REG {
 	uint16_t all;
 	struct SPI_SR_BITS bit;
 } SPI_SR_REG;
 
 
-typedef struct __attribute__((packed)) SPI_I2S_BITS {
+//SPI data register (SPI_DR)
+typedef union _SPI_DR_REG {
+	uint16_t all;
+} SPI_DR_REG;
+
+
+//SPI CRC polynomial register
+typedef union _SPI_CRCPR_REG {
+	uint16_t all;
+} SPI_CRCPR_REG;
+
+//SPI RX CRC register
+typedef union _SPI_RXCRCR_REG {
+	uint16_t all;
+} SPI_RXCRCR_REG;
+
+
+//SPI TX CRC register
+typedef union _SPI_TXCRCR_REG {
+	uint16_t all;
+} SPI_TXCRCR_REG;
+
+
+//SPI_I2S configuration bits
+typedef struct PACKED SPI_I2SCFGR_BITS {
 	unsigned CHLEN		:1; //Bit 0		Channel length (number of bits per audio channel)
 	unsigned DATLEN		:2; //Bits 2:1	Data length to be transferred
 	unsigned CKPOL		:1; //Bit 3		Steady state clock polarity
@@ -264,35 +299,52 @@ typedef struct __attribute__((packed)) SPI_I2S_BITS {
 	unsigned I2SE		:1; //Bit 10	I2S Enable
 	unsigned I2SMOD		:1; //Bit 11	I2S mode selection
 	unsigned RSVD_15_12	:4; //Bits 15:12 must be kept at reset value.
-} spi_i2s_t;
-static_assert(sizeof(spi_i2s_t) == SPI_I2S_T_SIZE, "Invalid size of spi_i2s_t!");
+} spi_i2scfgr_t;
+static_assert(sizeof(spi_i2scfgr_t) == SPI_I2SCFGR_T_SIZE, "Invalid size of spi_i2scfgr_t!");
 
+//SPI_I2S configuration register
 typedef union _SPI_I2S_REG {
 	uint16_t all;
-	struct SPI_I2S_BITS bit;
-} SPI_I2S_REG;
+	struct SPI_I2SCFGR_BITS bit;
+} SPI_I2SCFGR_REG;
+
+
+//SPI_I2S prescaler bits
+typedef struct PACKED SPI_I2SPR_BITS {
+	unsigned I2SDIV		:8; //Bits 7:0 I2S Linear prescaler
+	unsigned ODD		:1; //Bit 8 Odd factor for the prescaler
+	unsigned MCKOE		:1; //Bit 9 Master clock output enable
+	unsigned RSVD_10_15	:6; //Bits 15:10 Reserved
+} spi_i2spr_t;
+static_assert(sizeof(spi_i2spr_t) == SPI_I2SPR_T_SIZE, "Invalid size of spi_i2spr_t!");
+
+//SPI_I2S prescaler register
+typedef union _SPI_I2SPR_REG {
+	uint16_t all;
+	struct SPI_I2SPR_BITS bit;
+} SPI_I2SPR_REG;
 
 
 typedef struct
 {
-  __IO SPI_CR1_REG	CR1;		/*!< SPI control register 1 (not used in I2S mode),      Address offset: 0x00 */
-  uint16_t			RESERVED0;	/*!< Reserved, 0x02                                                           */
-  __IO SPI_CR2_REG	CR2;		/*!< SPI control register 2,                             Address offset: 0x04 */
-  uint16_t			RESERVED1;	/*!< Reserved, 0x06                                                           */
-  __IO SPI_SR_REG	SR;			/*!< SPI status register,                                Address offset: 0x08 */
-  uint16_t			RESERVED2;	/*!< Reserved, 0x0A                                                           */
-  __IO uint16_t		DR;			/*!< SPI data register,                                  Address offset: 0x0C */
-  uint16_t			RESERVED3;	/*!< Reserved, 0x0E                                                           */
-  __IO uint16_t		CRCPR;		/*!< SPI CRC polynomial register (not used in I2S mode), Address offset: 0x10 */
-  uint16_t			RESERVED4;	/*!< Reserved, 0x12                                                           */
-  __IO uint16_t		RXCRCR;		/*!< SPI RX CRC register (not used in I2S mode),         Address offset: 0x14 */
-  uint16_t			RESERVED5;	/*!< Reserved, 0x16                                                           */
-  __IO uint16_t		TXCRCR;		/*!< SPI TX CRC register (not used in I2S mode),         Address offset: 0x18 */
-  uint16_t			RESERVED6;	/*!< Reserved, 0x1A                                                           */
-  __IO SPI_I2S_REG	I2SCFGR;	/*!< SPI_I2S configuration register,                     Address offset: 0x1C */
-  uint16_t			RESERVED7;	/*!< Reserved, 0x1E                                                           */
-  __IO uint16_t		I2SPR;		/*!< SPI_I2S prescaler register,                         Address offset: 0x20 */
-  uint16_t			RESERVED8;	/*!< Reserved, 0x22                                                           */
+  __IO SPI_CR1_REG		CR1;		/*!< SPI control register 1 (not used in I2S mode),      Address offset: 0x00 */
+  uint16_t				RESERVED0;	/*!< Reserved, 0x02                                                           */
+  __IO SPI_CR2_REG		CR2;		/*!< SPI control register 2,                             Address offset: 0x04 */
+  uint16_t				RESERVED1;	/*!< Reserved, 0x06                                                           */
+  __IO SPI_SR_REG		SR;			/*!< SPI status register,                                Address offset: 0x08 */
+  uint16_t				RESERVED2;	/*!< Reserved, 0x0A                                                           */
+  __IO SPI_DR_REG		DR;			/*!< SPI data register,                                  Address offset: 0x0C */
+  uint16_t				RESERVED3;	/*!< Reserved, 0x0E                                                           */
+  __IO SPI_CRCPR_REG	CRCPR;		/*!< SPI CRC polynomial register (not used in I2S mode), Address offset: 0x10 */
+  uint16_t				RESERVED4;	/*!< Reserved, 0x12                                                           */
+  __IO SPI_RXCRCR_REG	RXCRCR;		/*!< SPI RX CRC register (not used in I2S mode),         Address offset: 0x14 */
+  uint16_t				RESERVED5;	/*!< Reserved, 0x16                                                           */
+  __IO SPI_TXCRCR_REG	TXCRCR;		/*!< SPI TX CRC register (not used in I2S mode),         Address offset: 0x18 */
+  uint16_t				RESERVED6;	/*!< Reserved, 0x1A                                                           */
+  __IO SPI_I2SCFGR_REG	I2SCFGR;	/*!< SPI_I2S configuration register,                     Address offset: 0x1C */
+  uint16_t				RESERVED7;	/*!< Reserved, 0x1E                                                           */
+  __IO SPI_I2SPR_REG	I2SPR;		/*!< SPI_I2S prescaler register,                         Address offset: 0x20 */
+  uint16_t				RESERVED8;	/*!< Reserved, 0x22                                                           */
 } spi_typedef;
 
 #endif /* INC_SPI_H_ */
